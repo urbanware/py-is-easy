76d25365100b705af8b296ee3a140cd22f7b84ca
Walter Spicer <git@urbanware.com>
2020-06-03 11:29:51 -0400

    init and start of hw01

---
+++ homework01/main.py
@@ -0,0 +1,98 @@
+'''
+For completion of Homework 01 at pirple.com
+
+I don't listen to music at all so I'll list favourite sci-fi books growing up.
+Also PEP-8 style guide refers to variable case as lower with underscores so I'll be using that.
+
+'''
+
+author = "David Brin"
+title = "Glory Season"
+genre = "Science Fiction"
+rating = 8.5
+year = 1992
+bsfa_award = False
+nebula_award = False
+isbn_10 = "0785754318"
+isbn_13 = "978-0-7857-5431-2"
+asin = "B00AA2O5RW"
+quick_review = "Questions society about clones and gender roles.  Has an interesting puzzle and mystery solving dynamic that I enjoyed but the ending was not fulfilling. Could have been the first book of a trilogy."
+
+
+author = "Robert Silverberg"
+title = "Lord Valentine's Castle"
+genre = "Fantasy"
+rating = 10.0
+year = 1980
+bsfa_award = False
+nebula_award = False
+isbn_10 = "1125594047"
+isbn_13 = "978-1125594049"
+asin = "B01A64FH4I"
+quick_review = "Was initially a trilogy then spawned another trilogy on the same planet at a different time.  Follow a juggler as he finds out he's Coronal and brings his world of Majipoor together. Great word salad terms yet you remain capitvated by the story and wish you could visit. "
+
+
+author = "Kim Stanley Robinson"
+title = "Red Mars"
+genre = "Hard Science Fiction"
+rating = 9.5
+year = 1992
+bsfa_award = True
+nebula_award = True
+isbn_10 = "0553560735"
+isbn_13 = "978-0553560732"
+asin = "B00HS7LI4A"
+quick_review = "Best novel on human colonization of Mars.  Gives a real sense of what it would take technically, the politics of environment, all in a realistic future.  Trilogy with Green Mars, Blue Mars."
+
+
+author = "Ray Bradbury"
+title = "Fahrenheit 451"
+genre = "Science Fiction"
+rating = 9.0
+year = 1953
+bsfa_award = False
+nebula_award = False
+isbn_10 = "9781451673319"
+isbn_13 = "978-1451673319"
+asin = "3453319834"
+quick_review = "Dystopian future about burning books to control information.  Tale about government control."
+
+
+author = "Arthur C. Clarke"
+title = "Rendevous with Rama"
+genre = "Hard Science Fiction"
+rating = 10.0
+year = 1973
+bsfa_award = True
+nebula_award = True
+isbn_10 = "0553287893"
+isbn_13 = "978-0553287899"
+asin = "B0073C34LK"
+quick_review = "Discover of intersteller ship in space. Later made into a series."
+
+
+author = "Barbara Hambly"
+title = "Dragonsbane"
+genre = "Fantasy"
+rating = 9.0
+year = 1985
+bsfa_award = None
+nebula_award = None
+isbn_10 = "0345349393"
+isbn_13 = "978-0345349392"
+asin = "B004TC149G"
+quick_review = "Love story between a wizard and a dragon."
+
+
+author = "Barbara Hambly"
+title = "The unschooled wizard"
+genre = "Fantasy"
+rating = 9.0
+year = 1987
+bsfa_award = None
+nebula_award = None
+isbn_10 = None
+isbn_13 = None
+asin = "B001MVXUA8"
+quick_review = "Sun Wolf and Starhawk adventures in wizardry."
+
